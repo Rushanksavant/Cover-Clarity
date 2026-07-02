f5182d463cb64051882e3f2c00167818a436b536
Rushanksavant <rushanksavant34@gmail.com>
2026-07-02 21:14:43 +0530

    build graph on cognee cloud

---
+++ graph/cloud/build_graph.py
@@ -0,0 +1,13 @@
+import sys
+import asyncio
+from executables import build, enrich
+
+async def main():
+    # Create Graph
+    await build(dry_run="--full" not in sys.argv)
+    
+    # Creates a semantic index that acts as an entry-point for queries
+    await enrich()
+
+if __name__ == "__main__":
+    asyncio.run(main())
